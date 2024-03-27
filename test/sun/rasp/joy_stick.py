6020d9041169d84319125d057052a2e5a3044713
sunjuwhan <mathsun0011@gmail.com>
2024-03-27 20:27:08 +0900

    Update joy_stick.py

--- test/sun/rasp/joy_stick.py
+++ test/sun/rasp/joy_stick.py
@@ -1,52 +1,32 @@
-#!/usr/bin/python
-#--------------------------------------
-# This script reads data from a
-# MCP3008 ADC device using the SPI bus.
-#
-# Analogue joystick version!
-#
-# Author : Matt Hawkins
-# Date   : 17/04/2014
-#
-# https://www.raspberrypi-spy.co.uk/
-#
-#--------------------------------------
- 
 import spidev
 import time
 import os
- 
-# Open SPI bus
-spi = spidev.SpiDev()
+
+spi = spidev.SpiDev()     
 spi.open(0,0)
- 
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
-def ReadChannel(channel):
+spi.max_speed_hz=1000000   
+
+def ReadChannel(channel):           
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
   return data
  
 # Define sensor channels
 # (channels 3 to 7 unused)
-swt_channel = 0
-vrx_channel = 1
-vry_channel = 2
- 
-# Define delay between readings (s)
-delay = 0.5
- 
+swt_channel = 0  
+vrx_channel = 1  
+vry_channel = 2  
+
+delay = 0.5  
+
 while True:
+
+  vrx_pos = ReadChannel(vrx_channel)  
+  vry_pos = ReadChannel(vry_channel) 
+
+  swt_val = ReadChannel(swt_channel)  
+
  
-  # Read the joystick position data
-  vrx_pos = ReadChannel(vrx_channel)
-  vry_pos = ReadChannel(vry_channel)
- 
-  # Read switch state
-  swt_val = ReadChannel(swt_channel)
- 
-  # Print out results
-  print("X : {}  Y : {}  Switch : {}".format(vrx_pos,vry_pos,swt_val))
- 
-  # Wait before repeating loop
+  print("X : {}  Y : {}  Switch : {}".format(vrx_pos,vry_pos,swt_val)) 
+
   time.sleep(delay)
