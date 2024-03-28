26811f3aa9375f7896836fae1eb3725be344572a
sunjuwhan <mathsun0011@gmail.com>
2024-03-28 12:54:14 +0900

    ip

---
+++ test/sun/server/HOST/re.py
@@ -0,0 +1,20 @@
+
+# 소켓 생성
+
+
+import socket
+
+#HOST='165.229.185.195'
+
+HOST='10.10.88.97'
+PORT =65432
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind((HOST,PORT))
+while True:
+    data,addr=sock.recvfrom(1024)
+    
+    print("수신한 데이터",data.decode(),'from',addr)
+
+
+        
--- test/sun/server/JOYSTICK/master.py
+++ test/sun/server/JOYSTICK/master.py
@@ -1,8 +1,8 @@
 import socket
 
 # 서버 주소와 포트
-#HOST = '165.229.185.195'
-HOST = '10.10.88.97'
+HOST = '165.229.185.195'
+#HOST = '10.10.88.97'
 PORT = 65432
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
