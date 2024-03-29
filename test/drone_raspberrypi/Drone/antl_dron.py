1c2cf6b414a937f93481c3037db275466ca38557
sunjuwhan <mathsun0011@gmail.com>
2024-03-29 23:46:43 +0900

    drone

--- test/drone_raspberrypi/Drone/antl_dron.py
+++ test/drone_raspberrypi/Drone/antl_dron.py
@@ -6,17 +6,17 @@ from Information.constant import *
 
 class drone():
     def __init__(self) -> None:
-        self.drone=System()
+        self.antl_drone=System()
     async def make_drone(self):
-        await self.drone.connect(system_address=VMWARE_SYSTEM_ADDRESS)
+        await self.antl_drone.connect(system_address=VMWARE_SYSTEM_ADDRESS)
         
         print("Wating for drone to connect...")  #drone connect 
-        async for state in self.drone.core.connection_state():
+        async for state in self.antl_drone.core.connection_state():
             if state.is_connected:
                 print(f"-- Connected to drone!")
                 break
         # Checking if Global Position Estimate is ok
-        async for health in self.drone.telemetry.health():
+        async for health in self.antl_drone.telemetry.health():
             if health.is_global_position_ok and health.is_home_position_ok:
                 print("-- Global position state is good enough for flying.")
                 break
@@ -24,12 +24,12 @@ class drone():
         await self.TakingOff()
     async def arming(self):
         print("-- Arming")
-        await self.drone.arm()
+        await self.antl_drone.arm()
     
     async def TakingOff(self):
         print("-- Taking off")
-        await self.drone.action.takeoff()
+        await self.antl_drone.action.takeoff()
         await asyncio.sleep(2)
     
-    def get_drone(self):
-        return self.drone
+    def get_drone(self) -> any:
+        return self.antl_drone
--- test/drone_raspberrypi/Manual/manual_mode.py
+++ test/drone_raspberrypi/Manual/manual_mode.py
@@ -7,11 +7,10 @@ class manual():
     def __init__(self,drone) -> None:
 
         self.antl_drone=drone  #main에서 drone객체 받아와서 조종 해야지
-        print("get_drone") 
     
     
     async def manul_controls(self,pitch,yaw,throttle,roll):
-        await self.antl_drone.manual_control.set_manual_control_input(
+        await self.antl_drone.get_drone().manual_control.set_manual_control_input(
            pitch,yaw,throttle,roll 
         )
         await asyncio.sleep(0.01)
