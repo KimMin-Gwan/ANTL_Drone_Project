4247e4e05d62d135a66a5faa3206e3b2c6412a16
sunjuwhan <mathsun0011@gmail.com>
2024-03-29 22:46:52 +0900

    Update main_function.py

--- test/drone_raspberrypi/main_function.py
+++ test/drone_raspberrypi/main_function.py
@@ -5,17 +5,23 @@ import Manual
 import socket
 import DroneController
 from threading import Thread
+import asyncio
 class Main_Function():
     def __init__(self) -> None:
         print("SYSTEM ALAM :: DRONE FUNCTION ON")
         self.Rx_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         self.Rx_socket.bind((Information.VMWARE_HOST,Information.VMWARE_PORT))  #받는거니까 내가 server로 여는거고
         self.Tx_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        self.drone=Drone.drone()
+        self.loop=asyncio.get_event_loop()
+        self.drone=None
+        self.loop.run_until_complete(self.create_drone_instance())
+        
         self.drone_controller=DroneController.controller(self.drone,self.Rx_socket)
         self.camera=Camera.camera(self.Tx_socket) 
         print("SYSTEM ALARM::Initializing Successfully Finished")
-    
+   
+    async def create_drone_instance(self):
+        self.drone=await Drone().drone()
     async def start_system(self):
         print("SYSTEM ALARM::System start")
         camera_thread=Thread(target=self.camera.send_FPV)
