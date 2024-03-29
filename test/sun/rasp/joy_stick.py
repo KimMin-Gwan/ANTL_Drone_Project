93c5a693e12782867333f0dc6c65ddb9717a7a03
sunjuwhan <mathsun0011@gmail.com>
2024-03-29 17:35:26 +0900

    Update joy_stick.py

--- test/sun/rasp/joy_stick.py
+++ test/sun/rasp/joy_stick.py
@@ -55,8 +55,8 @@ while True:
     vry_pos=500
   vrx_pos=(vrx_pos-500)/1000
   vry_pos=(vry_pos-500)/1000
-  vrx_pos= vrx_pos*90
-  vry_pos=vry_pos*90
+  vrx_pos= vrx_pos*60
+  vry_pos=vry_pos*60
   
   print("first    X : {}  Y : {}  Switch : {}".format(vrx_pos,vry_pos,swt_val)) 
   #print("second   X : {}  Y : {}  Switch : {}".format(vrx_pos_2,vry_pos_2,swt_val_2)) 
