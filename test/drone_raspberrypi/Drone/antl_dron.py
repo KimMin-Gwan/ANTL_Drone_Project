88bcc648c3dc146c9f360c57c8b5436e2b559214
sunjuwhan <mathsun0011@gmail.com>
2024-03-29 22:54:22 +0900

    Update antl_dron.py

--- test/drone_raspberrypi/Drone/antl_dron.py
+++ test/drone_raspberrypi/Drone/antl_dron.py
@@ -11,12 +11,12 @@ class drone():
         await self.drone.connect(system_address=VMWARE_SYSTEM_ADDRESS)
         
         print("Wating for drone to connect...")  #drone connect 
-        async for state in drone.core.connection_state():
+        async for state in self.drone.core.connection_state():
             if state.is_connected:
                 print(f"-- Connected to drone!")
                 break
         # Checking if Global Position Estimate is ok
-        async for health in drone.telemetry.health():
+        async for health in self.drone.telemetry.health():
             if health.is_global_position_ok and health.is_home_position_ok:
                 print("-- Global position state is good enough for flying.")
                 break
