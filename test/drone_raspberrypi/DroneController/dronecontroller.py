42e1ae0738ee53c644dfe45591cb83b7cd2121f3
sunjuwhan <mathsun0011@gmail.com>
2024-03-29 23:31:52 +0900

    Update dronecontroller.py

--- test/drone_raspberrypi/DroneController/dronecontroller.py
+++ test/drone_raspberrypi/DroneController/dronecontroller.py
@@ -14,8 +14,9 @@ class controller():
         self.throttle=None
         self.pitch=None
         self.yaw=None
-        self.mode_type=None        
+        self.mode_type=None       
         self.manual_drone=manual(drone)
+        print("make Manul")
         
     async def controll_dron(self):  #thread로 동작해야할 함수 
         #### Yaw Pitch Roll Throttle mode를 계속해서 받아서 mode에 따라서 드론 조종방식을 계속해서 바꾸어준다.
