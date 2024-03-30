cbb99dad12aa9c765ea7e6fd701d6b93dccf4425
sunjuwhan <mathsun0011@gmail.com>
2024-03-30 22:07:33 +0900

    model

--- main/drone/model/pilot_model.py
+++ main/drone/model/pilot_model.py
@@ -1,5 +1,27 @@
 
-
+import asyncio
+from mavsdk import System
+class Drone:
+    async def __init__(self) -> None:
+    #async def make_rone(self):
+        self.antl_drone=System()
+        await self.antl_drone.connect(system_address="udp://14540")
+        
+        print("Wating for drone to connect...")  #drone connect 
+        async for state in self.antl_drone.core.connection_state():
+            if state.is_connected:
+                print(f"-- Connected to drone!")
+                break
+        # Checking if Global Position Estimate is ok
+        async for health in self.antl_drone.telemetry.health():
+            if health.is_global_position_ok and health.is_home_position_ok:
+                print("-- Global position state is good enough for flying.")
+                break
+            
+        print("-- Arming")
+        await self.antl_drone.action.arm()
+    def get_drone(self):
+        return self.antl_drone
 class PilotModel:
     def __init__(self):
         self.__key = Key()
@@ -10,7 +32,7 @@ class PilotModel:
         return
 
     def set_key(self, key):
-
+        self.__key=key
         return 
     
     def get_key(self):
@@ -25,10 +47,14 @@ class Key:
         self.__throttle = 0
         self.__pitch = 0
         self.__roll = 0
-
     ##set
-        
+    def set_key(self,yaw,throttle,pitch,roll):
+        self.__yaw=yaw
+        self.__throttle=throttle
+        self.__pitch=pitch
+        self.__roll=roll
     ##get
-
+    def get_key(self):
+        return (self.__yaw,self.__throttle,self.__pitch,self.__roll)
 
 
