95b32e8ca6116ef82fd0412ab454e3f7b5e42385
sunjuwhan <mathsun0011@gmail.com>
2024-03-30 12:02:35 +0900

    Update antl_dron.py

--- test/drone_raspberrypi/Drone/antl_dron.py
+++ test/drone_raspberrypi/Drone/antl_dron.py
@@ -11,7 +11,7 @@ class drone():
         
         self.antl_drone=System()
         
-        await self.antl_drone.connect(system_address=VMWARE_SYSTEM_ADDRESS)
+        await self.antl_drone.connect(system_address="udp://:14540")
         
         print("Wating for drone to connect...")  #drone connect 
         async for state in self.antl_drone.core.connection_state():
