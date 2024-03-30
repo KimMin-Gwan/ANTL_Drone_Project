41aece8319d2b8f355f24a83a51604c538dc24a3
sunjuwhan <mathsun0011@gmail.com>
2024-03-30 14:48:45 +0900

    Update streaming.py

--- test/sun/real_sense/streaming.py
+++ test/sun/real_sense/streaming.py
@@ -5,7 +5,7 @@ def main():
     # RealSense pipeline 설정
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.brg8, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
     # RealSense 카메라 시작
     pipeline.start(config)
