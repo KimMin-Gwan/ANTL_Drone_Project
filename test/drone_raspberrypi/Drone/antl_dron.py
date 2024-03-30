ab39303d98f19acfda32cc2ce91495b56ab3390e
sunjuwhan <mathsun0011@gmail.com>
2024-03-30 12:03:39 +0900

    Update antl_dron.py

--- test/drone_raspberrypi/Drone/antl_dron.py
+++ test/drone_raspberrypi/Drone/antl_dron.py
@@ -23,6 +23,9 @@ class drone():
             if health.is_global_position_ok and health.is_home_position_ok:
                 print("-- Global position state is good enough for flying.")
                 break
+        await self.antl_drone.manual_control.set_manual_control_input(
+        float(0), float(0), float(0.5), float(0)
+    )
         await self.arming()
         await self.TakingOff()
     async def arming(self):
