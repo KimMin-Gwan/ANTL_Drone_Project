aef76e0cd63137bba903c7baab6869eb66c689e7
sunjuwhan <mathsun0011@gmail.com>
2024-03-30 22:09:08 +0900

    controll

--- main/drone/controller/pilot_controller.py
+++ main/drone/controller/pilot_controller.py
@@ -1,7 +1,22 @@
 from model.pilot_model import PilotModel
+from model.pilot_model import Drone
 
-
-
+import asyncio
+from mavsdk import System   
 class PilotController:
     def __init__(self):
         self.__pilot_model = PilotModel()
+        self.__drone=Drone()
+    async def run(self):
+        while True:
+            mode_type=self.__pilot_model.get_mode()
+            (yaw,throttle,pitch,roll)=self.__pilot_model.get_key()
+            
+            if mode_type=="manual":
+                await self.__drone.get_drone().manul_control.set_manual_control_input(pitch,yaw,throttle,roll)
+            elif mode_type=="land":
+                await self.__drone.get_drone().action.land()  #land 함수
+            elif mode_type=="detect":
+                pass
+        
+    
