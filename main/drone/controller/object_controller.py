3c7ae307e93df900cbb53ccfa4bc9b12af769112
sunjuwhan <mathsun0011@gmail.com>
2024-04-09 14:39:49 +0900

    Update object_controller.py

--- main/drone/controller/object_controller.py
+++ main/drone/controller/object_controller.py
@@ -31,9 +31,9 @@ class ObjectController:
         while True:
             print("start") 
             # 일시정지 상태
-            if not self.__mode.get_mode():
-                print("stop")
-                continue
+            # if not self.__mode.get_mode():
+            #     print("stop")
+            #     continue
 
             start_time = time.time()            
             frame = self.__video_model.get_raw_frame()
