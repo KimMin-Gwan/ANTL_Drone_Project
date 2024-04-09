5c6fe72822294837c93ae7d24f8b191bdf9c328d
sunjuwhan <mathsun0011@gmail.com>
2024-04-09 15:15:07 +0900

    Update common.py

--- main/drone/controller/common.py
+++ main/drone/controller/common.py
@@ -109,7 +109,7 @@ class Tools:
         for i in range(top_k):
             if scores[i] >= MIN_CONF_THRESHOLD:
                 obj = make(i)
-                if self.labels.get(obj.id) == 'person':
+                if self.labels.get(obj.id) == 'car':
                     filtered_objects.append(obj)
 
         return filtered_objects
