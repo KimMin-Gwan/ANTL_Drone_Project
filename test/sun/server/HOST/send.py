72f0be9a1c04e08d0382a39899e13e1ff2686687
sunjuwhan <mathsun0011@gmail.com>
2024-04-10 19:56:34 +0900

    test

--- test/sun/server/HOST/send.py
+++ test/sun/server/HOST/send.py
@@ -9,20 +9,22 @@ import sys
 
 
 
-HOST='127.0.0.1'
+HOST='165.229.185.195'
 server_port=5001
 #HOST='192.168.50.71'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST,server_port))
-sock.listen(1)
+sock.listen()
 
+client_socket,cliend_address=sock.accept()
 HOST_2='192.168.232.137'
 PORT_2=65433
 sock_2=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 while True:
-    data,addr=sock.recvfrom(1024)
+    data,addr=client_socket.recvfrom(1024)
+    print(data)
     sock_2.sendto(data,(HOST_2,PORT_2))
 
 
